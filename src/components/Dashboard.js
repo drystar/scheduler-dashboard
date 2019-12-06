c236e92ecbbe09db444672f19a17ad7ca1fef84f
drystar <chris.drysdale12@gmail.com>
2019-12-06 00:08:56 -0800

    importing helper functions to  compute data loaded from API server

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,26 +6,33 @@ import Loading from "./Loading";
 import Panel from "./panel";
 import axios from "axios";
 
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay
+} from "helpers/selectors";
+
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    getValue: getTotalInterviews
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    getValue: getLeastPopularTimeSlot
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    getValue: getMostPopularDay
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
+    getValue: getInterviewsPerDay
   }
 ];
 
@@ -89,7 +96,7 @@ class Dashboard extends Component {
           key={panel.key}
           id={panel.id}
           label={panel.label}
-          value={panel.value}
+          value={panel.getValue(this.state)}
           onSelect={() => this.selectPanel(panel.id)}
         />
       ));
